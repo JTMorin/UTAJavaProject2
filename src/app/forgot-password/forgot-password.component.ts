fdc5805ccf2ee899f4189cef98891aef19c91b55
Kevin Hedglin <kevinahedglin@gmail.com>
2019-04-10 17:33:38 -0500

    d

---
+++ src/app/forgot-password/forgot-password.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forgot-password',
+  templateUrl: './forgot-password.component.html',
+  styleUrls: ['./forgot-password.component.css',
+              '../styles/global/common.css',
+              '../styles/global/login.css']
+})
+export class ForgotPasswordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
