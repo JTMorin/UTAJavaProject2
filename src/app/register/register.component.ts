96514f040378ce4098de8b484b9735feca266af5
JTMorin <jarod.t.morin@gmail.com>
2019-04-12 15:18:40 -0500

    Testing with source

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css',
+              '../styles/global/common.css',
+              '../styles/global/login.css']
+})
+export class RegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
