14d7826a0f10f9dfe23725e6bc6b2e7741ba1592
Kevin Hedglin <kevinahedglin@gmail.com>
2019-04-15 10:58:00 -0500

    <commit_message>

--- app/register/register.component.ts
+++ app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-register',
@@ -9,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private elem: ElementRef) { 
+
+  }
 
   ngOnInit() {
   }
-
+  
 }
